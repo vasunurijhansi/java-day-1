f9a3d10669e652a00021f8143910fce6b410addd
vasunurijhansi <vasunurijhansi@gmail.com>
2026-01-31 16:02:32 +0530

    Add files via upload

---
+++ user3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class user3{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        System.out.println("enter age");
+        float a=sc.nextFloat();
+        System.out.println("enter a value");
+        double b=sc.nextDouble();
+        System.out.println("enter b value");
+        String str1=sc.next();
+        String str2=sc.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(str1);
+        System.out.println(age);
+    }
+}    
