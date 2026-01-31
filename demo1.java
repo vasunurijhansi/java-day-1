7eee7f6b7b1b7bf612df6a3bc4363808d3efb84c
vasunurijhansi <vasunurijhansi@gmail.com>
2026-01-31 14:28:06 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,10 @@
+class demo1{
+   public static void main(String args[]){ 
+    int a=10;
+    int b=20;
+    int c=a+b;
+    System.out.println(c);
+    c=a+b+a;
+    System.out.println(c);
+   }
+}
