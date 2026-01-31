d4cfb3eaa3c72ce707ae337c3c198cc8c742cdc4
vasunurijhansi <vasunurijhansi@gmail.com>
2026-01-31 15:41:16 +0530

    Add files via upload

---
+++ user2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class user2{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int self=sc.nextInt();
+        String str1=sc.nextLine();
+        System.out.println(str1);
+        System.out.println(self);
+    }
+}    
